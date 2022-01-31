e512f38c1403a96c0173e0213c4670724a5a6c39
jeremysz0419 <jeremiahszhuang@gmail.com>
2022-01-31 11:51:35 -0800

    adding geoJSON

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,7 +1,41 @@
 //check if logic is accessed by chrome
 console.log("working");
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map("mapid").setView([37.5, -122.5], 10);
+// Add GeoJSON data.
+let sanFranAirport = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        id: "3469",
+        name: "San Francisco International Airport",
+        city: "San Francisco",
+        country: "United States",
+        faa: "SFO",
+        icao: "KSFO",
+        alt: "13",
+        "tz-offset": "-8",
+        dst: "A",
+        tz: "America/Los_Angeles",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-122.375, 37.61899948120117],
+      },
+    },
+  ],
+};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(layer);
+    layer.bindPopup(feature.properties.city);
+  },
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
